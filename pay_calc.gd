d6fd1ef53d79f1994a62ed0c9ee393cbaaf243a7
TheAbsentTourist <118998365+TheAbsentTourist@users.noreply.github.com>
2026-06-12 15:46:14 -0400

    Fixed bug where percentages were only updated once on launch.

--- pay_calc.gd
+++ pay_calc.gd
@@ -12,10 +12,7 @@ extends Node2D
 @onready var savings_out: Label = $Out/SavingsOut
 @onready var invest_out: Label = $Out/InvestOut
 @onready var percent_out: Label = $PercentOut
-
-@onready var tithe_in: float = (tithe_input.value / 100.0)
-@onready var savings_in: float = (savings_input.value / 100.0)
-@onready var invest_in: float = (invest_input.value / 100.0)
+@onready var total_savings_invest_out: Label = $Out/TotalSavingsInvestOut
 
 var spending_amount: float = 0.0
 var tithe_amount: float = 0.0
@@ -24,6 +21,11 @@ var invest_amount: float = 0.0
 var invest_divided_amount: float = 0.0
 
 func _process(delta: float) -> void:
+	
+	var tithe_in: float = (tithe_input.value / 100.0)
+	var savings_in: float = (savings_input.value / 100.0)
+	var invest_in: float = (invest_input.value / 100.0)
+	
 	if (tithe_input.value + savings_input.value + invest_input.value) > 100.0:
 		savings_input.value = savings_input.value - 1
 		if savings_input.value == 0 and (tithe_input.value + savings_input.value + invest_input.value) > 100.0:
@@ -35,11 +37,22 @@ func _process(delta: float) -> void:
 	invest_divided_amount = invest_amount / invest_division_input.value
 	spending_amount = (pay_input.value - tithe_amount - savings_amount - invest_amount)
 	
+	update_outputs()
+
+
+
+func update_outputs():
+	percent_out.text = "Total Percent Taken: " + str("%1.0f" % (tithe_input.value + savings_input.value + invest_input.value)) + str("%")
+	
 	spending_out.text = "Spending: $" + str("%.2f" % spending_amount)
+	
 	tithe_out.text = "Tithe: $" + str("%.2f" % tithe_amount)
+	
 	savings_out.text = "Savings: $" + str("%.2f" % savings_amount)
-	if invest_divided_amount > 1:
+	
+	if invest_divided_amount < invest_amount:
 		invest_out.text = "Invest: $" + str("%.2f" % invest_amount) + "  |  " + str("%.2f" % invest_divided_amount) + "  x  " + str("%1.0f" % invest_division_input.value)
 	else:
 		invest_out.text = "Invest: $" + str("%.2f" % invest_amount)
-	percent_out.text = "Total Percent Taken: " + str("%1.0f" % (tithe_input.value + savings_input.value + invest_input.value)) + str("%")
+		
+	total_savings_invest_out.text = "Savings and Invest: $" +  str("%.2f" % (savings_amount + invest_amount))
